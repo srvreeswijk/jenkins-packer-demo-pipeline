36861d82f7d3cc43ff8e75e237d8117137e6707a
srvreeswijk <s.vreeswijk@gmail.com>
2018-10-24 21:37:08 +0200

    Update build.sh

--- jenkins-packer-demo-job1/build.sh
+++ jenkins-packer-demo-job1/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "we starten het build script"
-pwd
-ARTIFACT=`packer build -machine-readable packer-demo.json |awk -F, '$0 ~/artifact,0,id/ {print $6}'`
-AMI_ID=`echo $ARTIFACT | cut -d ':' -f2`
+packer build -machine-readable packer-example.json | tee build.log
+if [ $? > 0 ];then echo "the packer command has failed";exit 1;else echo "Packer succeded, continue...";fi
+AMI_ID=`grep -oPe 'ami-.+(?=\\n)' build.log |tail -n1`
 echo 'variable "APP_INSTANCE_AMI" { default = "'${AMI_ID}'" }' > amivar.tf
+cat amivar.tf
 # we copy the amivar.tf to s3 for part2 of this demo. It will use it to start the app. 
 aws s3 cp amivar.tf s3://terraform-state-a2b634h/packer-demo/amivar.tf
