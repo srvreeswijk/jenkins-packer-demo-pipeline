3ef9cf89ed7e50704238f91810a97dbc56666f77
srvreeswijk <s.vreeswijk@gmail.com>
2018-10-24 21:08:50 +0200

    Update build.sh

--- jenkins-packer-demo-job1/build.sh
+++ jenkins-packer-demo-job1/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "we starten het build script"
 ARTIFACT=`packer build -machine-readable packer-demo.json |awk -F, '$0 ~/artifact,0,id/ {print $6}'`
 AMI_ID=`echo $ARTIFACT | cut -d ':' -f2`
 echo 'variable "APP_INSTANCE_AMI" { default = "'${AMI_ID}'" }' > amivar.tf
