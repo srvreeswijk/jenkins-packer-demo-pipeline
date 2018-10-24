009c6f5882ceb32b06538a17c2d5504c2a0b7b05
srvreeswijk <s.vreeswijk@gmail.com>
2018-10-24 21:26:38 +0200

    Update build.sh

--- jenkins-packer-demo-job1/build.sh
+++ jenkins-packer-demo-job1/build.sh
@@ -5,4 +5,4 @@ ARTIFACT=`packer build -machine-readable packer-demo.json |awk -F, '$0 ~/artifac
 AMI_ID=`echo $ARTIFACT | cut -d ':' -f2`
 echo 'variable "APP_INSTANCE_AMI" { default = "'${AMI_ID}'" }' > amivar.tf
 # we copy the amivar.tf to s3 for part2 of this demo. It will use it to start the app. 
-aws s3 cp amivar.tf s3://terraform-state-a2b634h/amivar.tf
+aws s3 cp amivar.tf s3://terraform-state-a2b634h/packer-demo/amivar.tf
